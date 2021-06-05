6fb931319d0aff87b3002f72f2ef4198e80821fa
bgwinston <b.winston87@yahoo.com>
2021-06-04 22:18:06 -0500

    Code to calculate area of a circle.

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -7,13 +7,16 @@ public class Area {
         Scanner radius = new Scanner(System.in);   //Create Scanner Object
         System.out.println("What is the radius?");  //output for user
         int radius1 = radius.nextInt();//read user input
+        while (radius1 <= 0) {
+                System.out.println("This number is not allowed. Try again.");
+                radius1 = radius.nextInt();
+            }
+        double area = 3.14 * radius1 * radius1;   //calculate area
+        System.out.println("The area of the circle with a radius of " + radius1 + " is " + area);
+                return;
+            }
 
-        if (radius1 <= 0) {
-            System.out.println("No negative numbers allowed");
-        } else {
-            double area = 3.14 * radius1 * radius1;   //calculate area
-            System.out.println("The area of the circle with a radius of " + radius1 + " is " + area);
         }
 
-    }
-}
+
+
